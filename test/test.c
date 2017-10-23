c744ecbba7b5af7b0985be479c88c06ed8dca45e
Jarkko Piiroinen <jmp@git.mail.kapsi.fi>
2017-10-23 20:13:22 +0300

    Simplify tests.

--- test/test.c
+++ test/test.c
@@ -1,62 +1,50 @@
 #include "test.h"
 #include "array.h"
 
-static int test_array_create(void) {
+static void test_array_create(void) {
     Array *array = array_create();
     test_assert(array != NULL);
     test_assert(array_length(array) == 0);
     test_assert(array_capacity(array) > 0);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_create_no_memory(void) {
+static void test_array_create_no_memory(void) {
     Array *array;
     test_malloc_disable();
     array = array_create();
     test_malloc_enable();
     test_assert(array == NULL);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_create_no_memory_for_elements(void) {
+static void test_array_create_no_memory_for_elements(void) {
     Array *array;
     test_malloc_fail_after(1); /* Fail when allocating elements */
     array = array_create();
     test_malloc_enable();
     test_assert(array == NULL);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_length_of_empty(void) {
+static void test_array_length_of_empty(void) {
     Array *array = array_create();
     test_assert(array_length(array) == 0);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_length_of_null(void) {
+static void test_array_length_of_null(void) {
     test_assert(array_length(NULL) == 0);
-
-    return TEST_PASS;
 }
 
-static int test_array_insert_adds_null(void) {
+static void test_array_insert_adds_null(void) {
     Array *array = array_create();
     array_insert(array, 0, NULL);
     test_assert(array_get(array, 0) == 0);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_insert_out_of_bounds(void) {
+static void test_array_insert_out_of_bounds(void) {
     Array *array = array_create();
     test_assert(array_length(array) == 0);
     array_insert(array, -1, NULL);
@@ -64,11 +52,9 @@ static int test_array_insert_out_of_bounds(void) {
     array_insert(array, 1, NULL);
     test_assert(array_length(array) == 0);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_insert_increases_length(void) {
+static void test_array_insert_increases_length(void) {
     Array *array = array_create();
     test_assert(array_length(array) == 0);
     array_insert(array, 0, NULL);
@@ -80,17 +66,13 @@ static int test_array_insert_increases_length(void) {
     array_insert(array, 1, NULL);
     test_assert(array_length(array) == 4);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_insert_to_null(void) {
+static void test_array_insert_to_null(void) {
     test_assert(array_insert(NULL, 0, NULL) == 0);
-
-    return TEST_PASS;
 }
 
-static int test_array_insert_at_beginning(void) {
+static void test_array_insert_at_beginning(void) {
     int a[] = {1, 2, 3};
     Array *array = array_create();
     array_insert(array, 0, &a[0]);
@@ -100,11 +82,9 @@ static int test_array_insert_at_beginning(void) {
     test_assert(array_get(array, 1) == &a[1]);
     test_assert(array_get(array, 2) == &a[0]);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_insert_at_middle(void) {
+static void test_array_insert_at_middle(void) {
     int a[] = {1, 2, 3};
     Array *array = array_create();
     array_insert(array, 0, &a[0]);
@@ -114,11 +94,9 @@ static int test_array_insert_at_middle(void) {
     test_assert(array_get(array, 1) == &a[2]);
     test_assert(array_get(array, 2) == &a[1]);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_insert_at_end(void) {
+static void test_array_insert_at_end(void) {
     int a[] = {1, 2, 3};
     Array *array = array_create();
     array_insert(array, 0, &a[0]);
@@ -128,11 +106,9 @@ static int test_array_insert_at_end(void) {
     test_assert(array_get(array, 1) == &a[1]);
     test_assert(array_get(array, 2) == &a[2]);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_remove_from_beginning(void) {
+static void test_array_remove_from_beginning(void) {
     int a[] = {1, 2, 3};
     Array *array = array_create();
     array_push(array, &a[0]);
@@ -144,11 +120,9 @@ static int test_array_remove_from_beginning(void) {
     test_assert(array_get(array, 1) == &a[2]);
     test_assert(array_length(array) == 2);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_remove_from_middle(void) {
+static void test_array_remove_from_middle(void) {
     int a[] = {1, 2, 3};
     Array *array = array_create();
     array_push(array, &a[0]);
@@ -160,11 +134,9 @@ static int test_array_remove_from_middle(void) {
     test_assert(array_get(array, 1) == &a[2]);
     test_assert(array_length(array) == 2);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_remove_from_end(void) {
+static void test_array_remove_from_end(void) {
     int a[] = {1, 2, 3};
     Array *array = array_create();
     array_push(array, &a[0]);
@@ -176,11 +148,9 @@ static int test_array_remove_from_end(void) {
     test_assert(array_get(array, 1) == &a[1]);
     test_assert(array_length(array) == 2);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_remove_out_of_bounds(void) {
+static void test_array_remove_out_of_bounds(void) {
     int a[] = {1, 2, 3};
     Array *array = array_create();
     array_push(array, &a[0]);
@@ -190,11 +160,9 @@ static int test_array_remove_out_of_bounds(void) {
     test_assert(array_remove(array, 3) == NULL);
     test_assert(array_length(array) == 3);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_remove_decreases_length(void) {
+static void test_array_remove_decreases_length(void) {
     int a[] = {1, 2, 3};
     Array *array = array_create();
     array_push(array, &a[0]);
@@ -208,17 +176,13 @@ static int test_array_remove_decreases_length(void) {
     test_assert(array_remove(array, 0) == &a[2]);
     test_assert(array_length(array) == 0);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_remove_from_null(void) {
+static void test_array_remove_from_null(void) {
     test_assert(array_remove(NULL, 0) == NULL);
-
-    return TEST_PASS;
 }
 
-static int test_array_push_adds_to_end(void) {
+static void test_array_push_adds_to_end(void) {
     int a[] = {1, 2};
     Array *array = array_create();
     array_push(array, &a[0]);
@@ -226,22 +190,18 @@ static int test_array_push_adds_to_end(void) {
     array_push(array, &a[1]);
     test_assert(array_last(array) == &a[1]);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_push_adds_null(void) {
+static void test_array_push_adds_null(void) {
     Array *array = array_create();
     test_assert(array_length(array) == 0);
     test_assert(array_push(array, NULL) == 1);
     test_assert(array_get(array, 0) == NULL);
     test_assert(array_length(array) == 1);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_push_increases_length(void) {
+static void test_array_push_increases_length(void) {
     Array *array = array_create();
     test_assert(array_length(array) == 0);
     array_push(array, NULL);
@@ -249,17 +209,13 @@ static int test_array_push_increases_length(void) {
     array_push(array, NULL);
     test_assert(array_length(array) == 2);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_push_to_null(void) {
+static void test_array_push_to_null(void) {
     test_assert(array_push(NULL, NULL) == 0);
-
-    return TEST_PASS;
 }
 
-static int test_array_capacity_increases(void) {
+static void test_array_capacity_increases(void) {
     int i, initial_capacity;
     Array *array = array_create();
     initial_capacity = array_capacity(array);
@@ -274,11 +230,9 @@ static int test_array_capacity_increases(void) {
     test_assert(array_push(array, NULL) == 1);
     test_assert(array_capacity(array) > initial_capacity);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_capacity_no_memory(void) {
+static void test_array_capacity_no_memory(void) {
     int i, initial_capacity;
     Array *array = array_create();
     initial_capacity = array_capacity(array);
@@ -291,25 +245,19 @@ static int test_array_capacity_no_memory(void) {
     test_realloc_enable();
     test_assert(array_capacity(array) == initial_capacity);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_capacity_of_empty(void) {
+static void test_array_capacity_of_empty(void) {
     Array *array = array_create();
     test_assert(array_capacity(array) > 0);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_capacity_of_null(void) {
+static void test_array_capacity_of_null(void) {
     test_assert(array_capacity(NULL) == 0);
-
-    return TEST_PASS;
 }
 
-static int test_array_pop_returns_element(void) {
+static void test_array_pop_returns_element(void) {
     int a[] = {1, 2};
     Array *array = array_create();
     array_push(array, &a[0]);
@@ -319,27 +267,21 @@ static int test_array_pop_returns_element(void) {
     test_assert((int*) array_pop(array) == &a[0]);
     test_assert(array_length(array) == 0);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_pop_from_empty(void) {
+static void test_array_pop_from_empty(void) {
     Array *array = array_create();
     test_assert(array_length(array) == 0);
     test_assert((int*) array_pop(array) == NULL);
     test_assert(array_length(array) == 0);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_pop_from_null(void) {
+static void test_array_pop_from_null(void) {
     test_assert(array_pop(NULL) == NULL);
-    
-    return TEST_PASS;
 }
 
-static int test_array_get(void) {
+static void test_array_get(void) {
     int a[] = {1, 2};
     Array *array = array_create();
     array_push(array, &a[0]);
@@ -347,11 +289,9 @@ static int test_array_get(void) {
     test_assert((int*) array_get(array, 0) == &a[0]);
     test_assert((int*) array_get(array, 1) == &a[1]);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_get_out_of_bounds(void) {
+static void test_array_get_out_of_bounds(void) {
     int a[] = {1, 2};
     Array *array = array_create();
     array_push(array, &a[0]);
@@ -359,63 +299,49 @@ static int test_array_get_out_of_bounds(void) {
     test_assert((int*) array_get(array, -1) == NULL);
     test_assert((int*) array_get(array, 2) == NULL);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_get_from_empty(void) {
+static void test_array_get_from_empty(void) {
     Array *array = array_create();
     test_assert(array_get(array, -1) == NULL);
     test_assert(array_get(array, 0) == NULL);
     test_assert(array_get(array, 1) == NULL);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_get_from_null(void) {
+static void test_array_get_from_null(void) {
     test_assert(array_get(NULL, -1) == NULL);
     test_assert(array_get(NULL, 0) == NULL);
     test_assert(array_get(NULL, 1) == NULL);
-
-    return TEST_PASS;
 }
 
-static int test_array_set(void) {
+static void test_array_set(void) {
     int a[] = {1, 2};
     Array *array = array_create();
     array_push(array, &a[0]);
     test_assert(array_get(array, 0) == &a[0]);
     test_assert(array_set(array, 0, &a[1]) == 1);
     test_assert(array_get(array, 0) == &a[1]);
-
-    return TEST_PASS;
 }
 
-static int test_array_set_out_of_bounds(void) {
+static void test_array_set_out_of_bounds(void) {
     Array *array = array_create();
     test_assert(array_set(array, -1, NULL) == 0);
     test_assert(array_set(array, 1, NULL) == 0);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_set_in_empty(void) {
+static void test_array_set_in_empty(void) {
     Array *array = array_create();
     test_assert(array_set(array, 0, NULL) == 0);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_set_in_null(void) {
+static void test_array_set_in_null(void) {
     test_assert(array_set(NULL, 0, NULL) == 0);
-
-    return TEST_PASS;
 }
 
-static int test_array_last(void) {
+static void test_array_last(void) {
     int a[] = {1, 2};
     Array *array = array_create();
     array_push(array, &a[0]);
@@ -423,22 +349,16 @@ static int test_array_last(void) {
     array_push(array, &a[1]);
     test_assert((int*) array_last(array) == &a[1]);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_last_of_empty(void) {
+static void test_array_last_of_empty(void) {
     Array *array = array_create();
     test_assert((int*) array_last(array) == NULL);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_last_of_null(void) {
+static void test_array_last_of_null(void) {
     test_assert(array_last(NULL) == NULL);
-
-    return TEST_PASS;
 }
 
 /* For sorting test */
@@ -446,7 +366,7 @@ static int compare(const void *a, const void *b) {
     return **(int**) a - **(int**) b;
 }
 
-static int test_array_sort(void) {
+static void test_array_sort(void) {
     int a[] = {8, 3, 25, 87, 2, 17, 5, 9, 16};
     Array *array = array_create();
     array_push(array, &a[0]); /*  8 */
@@ -468,19 +388,15 @@ static int test_array_sort(void) {
     test_assert(array_get(array, 6) == &a[5]);
     test_assert(array_get(array, 7) == &a[2]);
     test_assert(array_get(array, 8) == &a[3]);
-
-    return TEST_PASS;
 }
 
-static int test_array_sort_empty(void) {
+static void test_array_sort_empty(void) {
     Array *array = array_create();
     array_sort(array, compare);
     array_free(array);
-
-    return TEST_PASS;
 }
 
-static int test_array_sort_null(void) {
+static void test_array_sort_null(void) {
     int a[] = {1, 2};
     Array *array = array_create();
     array_push(array, &a[0]);
@@ -489,8 +405,6 @@ static int test_array_sort_null(void) {
     array_sort(array, NULL);
     array_sort(NULL, NULL);
     array_free(array);
-
-    return TEST_PASS;
 }
 
 int main(void) {
@@ -539,5 +453,5 @@ int main(void) {
     test_run(test_array_sort_null);
     test_print_stats();
 
-    return test_get_fail_count();
+    return test_get_status();
 }
